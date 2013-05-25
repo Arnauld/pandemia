aff2b7fd8483c0c5247714ea2a4e4d83ead31625
Arnauld Loyer <arnauld.loyer@gmail.com>
2013-05-26 00:24:45 +0200

    cleanup + redis/clojure discovery

---
+++ test/pandemia/redis_test.clj
@@ -0,0 +1,34 @@
+(ns pandemia.redis-test
+  (:use clojure.test)
+  (:require [pandemia.util :as util]
+            [pandemia.event :as event]
+            [taoensso.carmine :as redis]
+            [clojure.tools.reader.edn :as edn]))
+
+(defn to-edn [obj] (pr-str obj))
+(defn from-edn [str] (edn/read-string str))
+
+
+(def pool         (redis/make-conn-pool)) ; See docstring for additional options
+(def spec-server1 (redis/make-conn-spec)) ; ''
+
+(defmacro wredis [& body] `(redis/with-conn pool spec-server1 ~@body))
+
+(deftest event->edn->redis
+  (testing "FIXME, If I fail."
+      (println "ppol: " pool)
+      (println "spec-server1: " spec-server1)
+      (let [uuid (util/uuid)
+            event {:event-type :pandemia.game.CityInfectedEvent
+                   :aggregate-id "game-1"
+                   :color :blue
+                   :nb-cubes 1
+                   :city-id :Madrid}
+            serialized (to-edn event)
+            redis-res (wredis 
+                          (redis/ping)
+                          (redis/rpush uuid serialized)
+                          (redis/lrange uuid 0 -1))
+            event-list (nth redis-res 2)
+            retrieved (first event-list)]
+        (is (= event (from-edn retrieved))))))
